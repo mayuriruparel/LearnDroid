f8360a4c3b7457c5ca603c1bf65c61cf5a089647
mayuriruparel <mayuri2411@gmail.com>
2012-09-24 18:07:42 +0530

    Added Splash Activity

---
+++ LearnDroid/src/com/mayuri/learndroid/Splash.java
@@ -0,0 +1,38 @@
+package com.mayuri.learndroid;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class Splash extends Activity {
+
+	private final int SPLASH_DISPLAY_LENGHT = 3000;
+
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        
+        /* New Handler to start the Menu-Activity 
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,Home.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_splash, menu);
+        return true;
+    }
+}
